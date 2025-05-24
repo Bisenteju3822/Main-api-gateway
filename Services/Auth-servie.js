0f1cd37aa4ffb9edce57fb2e3ac7de96c92b3ed5
Tejasave <tejasavebisen@gmail.com>
2025-05-24 08:37:54 +0530

    first commit

---
+++ Services/Auth-servie.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/auth', (req, res) => {
+  res.json({ message: 'Auth service: Login successful (GET)' });
+});
+
+app.post('/auths', (req, res) => {
+  res.json({ message: 'Auth service: Login successful (POST)', body: req.body });
+});
+
+app.listen(5007, "127.0.0.1", () => {
+  console.log('Auth service running on http://127.0.0.1:5007');
+});
