8d74ebe2406ed1d91b0afb696de5f9f1b642d397
Tejasave <tejasavebisen@gmail.com>
2025-05-29 00:31:52 +0530

    new

--- Services/Auth-servie.js
+++ Services/Auth-servie.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
+
 app.use(express.json());
 
 app.get('/auth', (req, res) => {
-  res.json({ message: 'Auth service: Login successful (GET)' });
+  res.json({ service: 'auth-multiport', message: 'GET: Auth success' });
 });
 
-app.post('/auths', (req, res) => {
-  res.json({ message: 'Auth service: Login successful (POST)', body: req.body });
+app.post('/auth/login', (req, res) => {
+  res.json({ service: 'auth-multiport', message: 'POST: Auth login success', body: req.body });
 });
 
-app.listen(5007, "127.0.0.1", () => {
-  console.log('Auth service running on http://127.0.0.1:5007');
+const ports = [5007, 5008, 5009];
+
+ports.forEach(port => {
+  app.listen(port, () => {
+    console.log(`Auth server listening on port ${port}`);
+  });
 });
